f3df1a359d816139ba157bc7c82a2f8c22b5a31f
fabien-grenon-cbp <fabien.grenon@cbp-group.com>
2018-12-14 15:12:23 +0100

    =init

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+
+// import InternalComponent from './components/InternalComponent/InternalComponent';
+import ExternalComponent from '../node_modules/@fabien.branchel/shelby/src/components/ExternalComponent/ExternalComponent.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        App
+        <Router>
+          <div>
+            <Route exact path='/' component={ExternalComponent} />
+          </div>
+        </Router>
       </div>
     );
   }
 }
 
-export default App;
+export default connect()(App);
